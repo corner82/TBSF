4382db1b240f828c4f28b670777e2522a30cc262
corner82 <311corner82@gmail.com>
2017-09-13 11:01:41 +0300

    Custom user for Identity
    Custom user for Identity

--- TBSF.MVCWebUi/Entities/CustomIdentityUser.cs
+++ TBSF.MVCWebUi/Entities/CustomIdentityUser.cs
@@ -3,12 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace TBSF.MVCWebUi.Entities
 {
-    public class CustomIdentityUser:IdentityUser
+    public class CustomIdentityUser : IdentityUser
     {
         //public string UserName { get; set; }
         //public string Email { get; set; }
+        public static explicit operator CustomIdentityUser(ClaimsPrincipal v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
