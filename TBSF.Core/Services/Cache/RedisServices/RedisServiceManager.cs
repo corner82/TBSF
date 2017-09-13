e1414db93689ced0c24c1de662a7f689a3d3607b
corner82 <311corner82@gmail.com>
2017-09-13 10:32:45 +0300

    Redis service manager for CRUD cache operations
    Redis service manager for CRUD cache operations

---
+++ TBSF.Core/Services/Cache/RedisServices/RedisServiceManager.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBSF.Core.Services.Cache.RedisServices
+{
+    public class RedisServiceManager
+    {
+        private readonly IDistributedCache _distributedCache;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public RedisServiceManager(IDistributedCache distributedCache,
+                                   IHostingEnvironment hostingEnvironment)
+        {
+            _distributedCache = distributedCache;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public async Task AddToCache(string key, string jsonData)
+        {
+            try
+            {
+                await _distributedCache.SetStringAsync(key, jsonData);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                if(_hostingEnvironment.IsDevelopment())
+                {
+                    Console.WriteLine(ex.GetType().FullName);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            finally{
+
+            }
+            /*var GetMessagesCacheExpiryMinutes = 5;
+            var GetProfileCacheExpiryHours = 1;
+            _distributedCache.is
+            if (connectionMultiplexer.IsConnected)
+            {
+                var cache = connectionMultiplexer.GetDatabase();
+
+                TimeSpan expiresIn;
+                // Search Cache
+                if (key.Contains("-"))
+                {
+                    expiresIn = new TimeSpan(0, GetMessagesCacheExpiryMinutes, 0);
+                }
+                // User info cache
+                else
+                {
+                    expiresIn = new TimeSpan(GetProfileCacheExpiryHours, 0, 0);
+                }
+                await cache.StringSetAsync(key, serializedData, expiresIn).ConfigureAwait(false);
+
+            }*/
+        }
+    }
+}
