524a59106f13c0bb9d01b75b9024d25a0821c0f8
corner82 <311corner82@gmail.com>
2017-09-13 11:00:35 +0300

    project changes
    project changes

--- TBSF.WebAPITest/Startup.cs
+++ TBSF.WebAPITest/Startup.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TBSF.WebAPITest.Middlewares;
+using Microsoft.AspNetCore.Http;
+using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing;
+using Microsoft.AspNetCore.HttpOverrides;
+using TBSF.Core.Services.RemoteAddressServices;
+using TBSF.MVCWebUi.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace TBSF.WebAPITest
 {
@@ -36,8 +43,30 @@ namespace TBSF.WebAPITest
                                    .AllowCredentials();
                         });
             });
+
+            //services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddDbContext<CustomIdentityDbContext>(options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<CustomIdentityUser, CustomIdentityRole>()
+                            .AddEntityFrameworkStores<CustomIdentityDbContext>()
+                            .AddDefaultTokenProviders();
+
+            services.AddSession();
+            services.AddDistributedRedisCache(options =>
+            {
+                options.InstanceName = Configuration.GetConnectionString("RedisInstanceName");
+                //options.Configuration = "185.86.4.73:6379,password=aaal123";
+                options.Configuration = Configuration.GetConnectionString("RedisServer");
+
+                //options.Configuration = "localhost";
+            });
+
             // Add framework services.
             services.AddMvc();
+            services.AddSingleton<Microsoft.AspNetCore.Http.IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<RemoteAddressService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +74,21 @@ namespace TBSF.WebAPITest
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            /*IHttpContextAccessor httpContextAccessor = app.ApplicationServices.GetRequiredService<IHttpContextAccessor>();
+            Extensions.Context.Configure(httpContextAccessor);*/
+
+            // use here to get ip adress of client on kestrel or nginx production servers
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseSession();
+            app.UseIdentity();
+
+            // extended middleware for HMC calculation
+            app.UseHMACModules();
 
             app.UseCors("AllowAll");
             app.UseMvc();
