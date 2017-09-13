6513e8892476b1ca9c63486787ff764469c41b58
corner82 <311corner82@gmail.com>
2017-09-13 10:30:53 +0300

    Extension methods for string manipulations
    Extension methods for string manipulations used for varios purpose

---
+++ TBSF.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TBSF.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static List<string> SplitCsv(this string csvList, bool nullOrWhitespaceInputReturnsNull = false)
+        {
+            if (string.IsNullOrWhiteSpace(csvList))
+                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
+
+            return csvList
+                .TrimEnd(',')
+                .Split(',')
+                .AsEnumerable<string>()
+                .Select(s => s.Trim())
+                .ToList();
+        }
+
+        public static bool IsNullOrWhitespace(this string s)
+        {
+            return String.IsNullOrWhiteSpace(s);
+        }
+
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return String.IsNullOrEmpty(s);
+        }
+    }
+}
